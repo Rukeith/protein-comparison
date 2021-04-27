e81bb036ac20d4dfa4c58b4cb50f8891f05f5eb7
Edwin Jarvis <fenglan.crazydance@gmail.com>
2021-04-27 21:49:59 +0800

    feat(table): update table structure
    Signed-off-by: Edwin Jarvis <fenglan.crazydance@gmail.com>

--- components/Table.vue
+++ components/Table.vue
@@ -10,9 +10,9 @@
           :key="i"
           :data="tr"
           sort
-          @click="proteins = $vs.sortData($event, proteins, 'id')"
+          @click="proteins = $vs.sortData($event, proteins, tr)"
         >
-          {{ tr }}
+          {{ $t(tr) }}
         </vs-th>
       </vs-tr>
     </template>
@@ -22,118 +22,79 @@
         :key="i"
         :data="tr"
       >
-        <vs-td v-for="(val, kw) in tr" :key="kw">
-          {{ val }}
-        </vs-td>
+        <vs-td>{{ tr.brand }}</vs-td>
+        <vs-td
+          ><a :href="tr.link">{{ tr.name }}</a></vs-td
+        >
+        <vs-td>{{ tr.price }}</vs-td>
+        <vs-td>{{ tr.protein }}</vs-td>
+        <vs-td>{{ tr.carbohydrates }}</vs-td>
+        <vs-td>{{ tr.dietary_fiber || '' }}</vs-td>
+        <vs-td>{{ tr.fat }}</vs-td>
+        <vs-td>{{ tr.sodium }}</vs-td>
+        <vs-td>{{ tr.BCAAs || '' }}</vs-td>
+
+        <template #expand>
+          <div>
+            <strong>其他額外成分</strong>
+            <li v-for="(val, kw) in tr.others" :key="kw">
+              {{ $t(kw) }}: {{ val }}
+            </li>
+          </div>
+        </template>
       </vs-tr>
     </template>
   </vs-table>
 </template>
 
 <script>
+import proteins from '~/static/data.json'
+
+proteins.forEach((element) => {
+  for (const [key, value] of Object.entries(element)) {
+    let count = value
+    if (key === 'price') {
+      count = Math.round(
+        (35 / (element.serving * element.weight)) * element.price
+      )
+    } else if (
+      ![
+        'brand',
+        'name',
+        'link',
+        'price',
+        'weight',
+        'serving',
+        'others',
+      ].includes(key)
+    ) {
+      count = Math.round(0.35 * value)
+    }
+    element[key] = count
+  }
+
+  if ('others' in element) {
+    for (const [key, value] of Object.entries(element.others)) {
+      element[key] = Math.round(0.35 * value)
+    }
+  }
+})
+
 export default {
   data: () => ({
     search: '',
     columns: [
-      '廠商',
-      '品名',
-      '單價',
-      '碳水化合物/膳食纖維(g)',
-      '蛋白質(g)',
-      '脂肪(g)',
-      '鈉',
-      'BCAAs(g)',
-      '鈣',
-      '磷',
-      '鎂',
-      '鉀',
-      '氯',
-      '麩胺酸',
-      '鎂',
-      '維生素D(μg)',
-      '維生素B1',
-      '維生素B2',
-      '維生素B6',
-      '維生素B12(μg)',
-      '精氨酸',
-      '牛磺酸',
-      '穀氨酸能',
-      '必要氨基酸',
-      '非必要氨基酸',
-      '甜菜根萃取物',
+      'brand',
+      'name',
+      'price',
+      'protein',
+      'carbohydrates',
+      'dietary_fiber',
+      'fat',
+      'sodium',
+      'BCAAs',
     ],
-    proteins: [
-      {
-        id: 1,
-        name: 'Leanne Graham',
-        username: 'Bret',
-        email: 'Sincere@april.biz',
-        website: 'hildegard.org',
-      },
-      {
-        id: 2,
-        name: 'Ervin Howell',
-        username: 'Antonette',
-        email: 'Shanna@melissa.tv',
-        website: 'anastasia.net',
-      },
-      {
-        id: 3,
-        name: 'Clementine Bauch',
-        username: 'Samantha',
-        email: 'Nathan@yesenia.net',
-        website: 'ramiro.info',
-      },
-      {
-        id: 4,
-        name: 'Patricia Lebsack',
-        username: 'Karianne',
-        email: 'Julianne.OConner@kory.org',
-        website: 'kale.biz',
-      },
-      {
-        id: 5,
-        name: 'Chelsey Dietrich',
-        username: 'Kamren',
-        email: 'Lucio_Hettinger@annie.ca',
-        website: 'demarco.info',
-      },
-      {
-        id: 6,
-        name: 'Mrs. Dennis Schulist',
-        username: 'Leopoldo_Corkery',
-        email: 'Karley_Dach@jasper.info',
-        website: 'ola.org',
-      },
-      {
-        id: 7,
-        name: 'Kurtis Weissnat',
-        username: 'Elwyn.Skiles',
-        email: 'Telly.Hoeger@billy.biz',
-        website: 'elvis.io',
-      },
-      {
-        id: 8,
-        name: 'Nicholas Runolfsdottir V',
-        username: 'Maxime_Nienow',
-        email: 'Sherwood@rosamond.me',
-        website: 'jacynthe.com',
-      },
-      {
-        id: 9,
-        name: 'Glenna Reichert',
-        username: 'Delphine',
-        email: 'Chaim_McDermott@dana.io',
-        website: 'conrad.com',
-      },
-      {
-        id: 10,
-        name: 'Clementina DuBuque',
-        username: 'Moriah.Stanton',
-        email: 'Rey.Padberg@karina.biz',
-        website: 'ambrose.net',
-      },
-    ],
+    proteins,
   }),
 }
 </script>
